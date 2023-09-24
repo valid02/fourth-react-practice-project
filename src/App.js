3bdc41325d9e9282bd9068281eeb8f77a5b5fd2a
Valid Rabiee <validrabieejs@gmail.com>
2023-09-24 12:25:41 +0330

    introduction to useEffect Hook

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  // رویکرد زیر کار میکند اما یک مشکل اساسی دارد و آن این است که یک حلقه بینهایت درست میکند
+  // هر بار که کامپوننت ما بر اثر عملیاتی دوباره پردازش شود این کد ما هم دوباره و دوباره اجرا میشود و این اصلا بهینه نیست
+  // است useEffect Hook راه حل بهینه برای اینگونه موارد
+  if (storedUserLoggedInInformation === '1') {
+    setIsLoggedIn(true);
+  }
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
